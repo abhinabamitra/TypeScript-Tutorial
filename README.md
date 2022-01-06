4eece61ab53a7bf7562319e0a5dac7c65681852e
troj4n <abhinabamitra19@gmail.com>
2022-01-06 22:15:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TypeScript-Tutorial
+ A beginner's attempt at typescript (Creds: The Net Ninja)
---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const character = 'abhi';
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input)
+});
